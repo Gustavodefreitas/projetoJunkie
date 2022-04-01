bc1bb65b328d3fa4a3bfb1b668dfddd98eae1447
Gustavo <ovatsug.gu@hotmail.com>
2022-04-01 03:10:38 -0300

    primeiro commit junkie

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View,Image} from 'react-native';
+import logoImage from './assets/junkie.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Image style={styles.logo} source={logoImage}/>
       <StatusBar style="auto" />
+      <View style={styles.TittleText}> Junkie Lab co.</View>
     </View>
+
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#C0C0C0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    height:400,
+    width: 400
+  },
+  TittleText:{
+
+    fontSize : 40,
+    fontWeight:'bold',
+    //marginBottom:200
+  }
 });
