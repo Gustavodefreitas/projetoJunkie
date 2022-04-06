89ede8e2ed85bd3bf96e765ffaaadf2f2652ed4d
Gustavo <ovatsug.gu@hotmail.com>
2022-04-06 14:52:56 -0300

    screens

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,Image} from 'react-native';
-import logoImage from './assets/junkie.png';
+import {SplashScreen} from './src/screens/SplashScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-      <Image style={styles.logo} source={logoImage}/>
-      <StatusBar style="auto" />
-      <View style={styles.TittleText}> Junkie Lab co.</View>
-    </View>
-
+    <SplashScreen/>
    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#C0C0C0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    height:400,
-    width: 400
-  },
-  TittleText:{
-
-    fontSize : 40,
-    fontWeight:'bold',
-    //marginBottom:200
-  }
-});
---
+++ src/screens/SplashScreen/index.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,Image} from 'react-native';
+import logoImage from '../../assets/junkie.png';
+
+export const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      
+      <Image style={styles.logo} source={logoImage}/>
+      <StatusBar style="auto" />
+      <View style={styles.TittleText}> Junkie Lab co.</View>
+    </View>
+
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C0C0C0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    height:400,
+    width: 400
+  },
+  TittleText:{
+
+    fontSize : 40,
+    fontWeight:'bold',
+    //marginBottom:200
+  }
+});
